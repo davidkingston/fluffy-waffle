be5b78526a1657f16f4c5960fa31089b364e6ce5
Dave Kingston <kingston34232@gmail.com>
2016-12-19 05:47:43 -0500

    Added the Mentor provider.

---
+++ app/src/main/java/com/example/wgu/MentorProvider.java
@@ -0,0 +1,69 @@
+package com.example.wgu;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.content.UriMatcher;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
+
+public class MentorProvider extends ContentProvider {
+    public static final String CONTENT_ITEM_TYPE = "Mentor";
+    private static final String AUTHORITY = "com.example.wgu.mentorprovider";
+    private static final String BASE_PATH = "asessment";
+    public static final Uri MENTOR_CONTENT_URI = Uri.parse("content://" + AUTHORITY + "/" + BASE_PATH);
+    private static final int MENTORS_BY_COURSE_ID = 1;
+    private static final int MENTOR_BY_ID = 2;
+    private static final UriMatcher uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+
+    static {
+        uriMatcher.addURI(AUTHORITY, BASE_PATH + "/c/#", MENTORS_BY_COURSE_ID);
+        uriMatcher.addURI(AUTHORITY, BASE_PATH + "/#", MENTOR_BY_ID);
+    }
+
+    private SQLiteDatabase db;
+
+    @Override
+    public boolean onCreate() {
+        DBHelper helper = new DBHelper(getContext());
+        db = helper.getWritableDatabase();
+        return true;
+    }
+
+    @Override
+    public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
+        if (uriMatcher.match(uri) == MENTOR_BY_ID) {
+            selection = DBHelper.COLUMN_MENTOR_ID + "=" + uri.getLastPathSegment();
+        } else {
+            selection = DBHelper.COLUMN_MENTOR_COURSE_ID + "=" + uri.getLastPathSegment();
+        }
+
+        return db.query(
+                DBHelper.TABLE_MENTOR,
+                DBHelper.ALL_MENTOR_COLUMNS,
+                selection,
+                null, null, null,
+                DBHelper.COLUMN_MENTOR_NAME);
+    }
+
+    @Override
+    public String getType(Uri uri) {
+        return null;
+    }
+
+    @Override
+    public Uri insert(Uri uri, ContentValues contentValues) {
+        long id = db.insert(DBHelper.TABLE_MENTOR, null, contentValues);
+        return Uri.parse(BASE_PATH + "/" + id);
+    }
+
+    @Override
+    public int update(Uri uri, ContentValues contentValues, String selection, String[] selectionArgs) {
+        return db.update(DBHelper.TABLE_MENTOR, contentValues, selection, selectionArgs);
+    }
+
+    @Override
+    public int delete(Uri uri, String selection, String[] selectionArgs) {
+        return db.delete(DBHelper.TABLE_MENTOR, selection, selectionArgs);
+    }
+}
