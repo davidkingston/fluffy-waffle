797fa5c648130f55e033bc7f1b69c6d347c27daa
Dave Kingston <kingston34232@gmail.com>
2017-01-02 16:31:05 -0500

    Added notes to the sample data.

--- app/src/main/java/com/example/wgu/AssessmentListActivity.java
+++ app/src/main/java/com/example/wgu/AssessmentListActivity.java
@@ -119,6 +119,7 @@ public class AssessmentListActivity extends AppCompatActivity
         ContentValues values = new ContentValues();
         values.put(DBHelper.COLUMN_ASSESSMENT_TITLE, title);
         values.put(DBHelper.COLUMN_ASSESSMENT_COURSE_ID, courseId);
+        values.put(DBHelper.COLUMN_ASSESSMENT_NOTE, "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
         getContentResolver().insert(AssessmentProvider.CONTENT_URI, values);
     }
 
